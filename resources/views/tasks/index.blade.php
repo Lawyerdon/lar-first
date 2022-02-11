cde68248db3f0b874c0a071559ea711e685efc8b
Artem Stasenko <85448490+edorofeev89@users.noreply.github.com>
2022-02-11 17:48:00 +0200

    views and actions

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <!-- Bootstrap шаблон... -->
+
+    <a href="{{ route('tasks.create') }}" class="btn btn-success">Create new task</a>
+
+    <!-- Текущие задачи -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Текущая задача
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- Заголовок таблицы -->
+                    <thead>
+                    <th>Task</th>
+                    <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- Тело таблицы -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- Имя задачи -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <td>
+                                <!-- TODO: Кнопка Удалить -->
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+@endsection
